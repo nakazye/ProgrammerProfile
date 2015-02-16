b72961fac64567e42f827437e1b993612f83abeb
nakazye <nakazye@gmail.com>
2015-02-16 23:30:42 +0900

    close #8
    add sitemap

--- ProgrammerProfile/settings.py
+++ ProgrammerProfile/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'social.apps.django_app.default',
     'ProgrammerProfile',
     'gunicorn',
--- ProgrammerProfile/urls.py
+++ ProgrammerProfile/urls.py
@@ -2,6 +2,13 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from ProgrammerProfile import sitemaps as my_sitemaps
+
+sitemaps = {'index': my_sitemaps.IndexSitemap,
+            'stats': my_sitemaps.StatsSitemap,
+            'staruser': my_sitemaps.StaruserSitemap,
+            'user': my_sitemaps.UserSitemap}
 
 urlpatterns = patterns('',
     url('^admin/',
@@ -24,5 +31,7 @@ urlpatterns = patterns('',
         'ProgrammerProfile.views.logout'),
     url('',
         include('social.apps.django_app.urls', namespace='social')),
+    url('^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ) + static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT)
